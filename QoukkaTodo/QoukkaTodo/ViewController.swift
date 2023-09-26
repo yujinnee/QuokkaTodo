9f573d826a51748babea9671150576e14e2f4350
Yujin Lee <yj587878@gmail.com>
2023-09-27 00:51:15 +0900

    [ADD] 프로젝트 생성

---
+++ QoukkaTodo/QoukkaTodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QoukkaTodo
+//
+//  Created by 이유진 on 2023/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
