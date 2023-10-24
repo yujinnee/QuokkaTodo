0a9363878e89e1b7006f9e840fc62ada8de8a935
Yujin Lee <yj587878@gmail.com>
2023-10-24 21:04:20 +0900

    [FEAT] removeObserver 추가

--- QuokkaTodo/QuokkaTodo/ViewControllers/Todo/TodoViewController.swift
+++ QuokkaTodo/QuokkaTodo/ViewControllers/Todo/TodoViewController.swift
@@ -237,6 +237,10 @@ class TodoViewController: BaseViewController{
         }
         
     }
+    deinit {
+           NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+           NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+       } 
     
     func setDelegate(){
         todoCollectionView.delegate = self
